2bfdf85e5a3bf9e6ec108849609f106c5938a397
jannat-jui <jannat15-13735@diu.edu.bd>
2023-11-05 20:14:50 -0800

    Implement JWT for verify the user

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 require('dotenv').config();
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middleware
-app.use(cors());
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+    ],
+    credentials: true
+}));
 app.use(express.json());
+app.use(cookieParser());
 
 
 
@@ -22,11 +30,54 @@ const client = new MongoClient(uri, {
   }
 });
 
+
+
+// middlewares 
+const logger = (req, res, next) => {
+    console.log('log: info', req.method, req.url);
+    next();
+}
+
+const verifyToken = (req, res, next) => {
+    const token = req?.cookies?.token;
+    if (!token) {
+        return res.status(401).send({ message: 'unauthorized access' })
+    }
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+        if (err) {
+            return res.status(401).send({ message: 'unauthorized access' })
+        }
+        req.user = decoded;
+        next();
+    })
+}
+
 async function run() {
   try {
     
     const foodCollection = client.db('sharefoodDB').collection('food');
 
+    // auth related api
+    app.post('/jwt', logger, async (req, res) => {
+        const user = req.body;
+        console.log('user for token', user);
+        const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' });
+
+        res.cookie('token', token, {
+            httpOnly: true,
+            secure: true,
+            sameSite: 'none'
+        })
+            .send({ success: true });
+    })
+
+    app.post('/logout', async (req, res) => {
+        const user = req.body;
+        console.log('logging out', user);
+        res.clearCookie('token', { maxAge: 0 }).send({ success: true })
+    })
+
+    
     // add new food related api
     app.post('/food', async (req, res) => {
         const newfood = req.body;
