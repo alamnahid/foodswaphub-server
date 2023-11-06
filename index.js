4ea4360ce12af7936daa269ee6ebbd607e7fe748
jannat-jui <jannat15-13735@diu.edu.bd>
2023-11-05 18:09:05 -0800

    Implement the post operation to store the food information is food collection in the database

--- index.js
+++ index.js
@@ -10,6 +10,46 @@ app.use(cors());
 app.use(express.json());
 
 
+
+const uri =`mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vgt34f5.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
+
+async function run() {
+  try {
+    
+    const foodCollection = client.db('sharefoodDB').collection('food');
+
+    // add new food related api
+    app.post('/food', async (req, res) => {
+        const newfood = req.body;
+        console.log(newfood);
+        const result = await foodCollection.insertOne(newfood);
+        res.send(result);
+      })
+
+
+
+    // await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    // await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+
+
+
 app.get('/', (req, res) => {
     res.send('Brand Shop server is running')
   })
